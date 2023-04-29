c8ac345a5f1d016dc45ec7d459de32910457ba7b
Vlad <vladislav.osadchy@gmail.com>
2023-04-29 22:38:20 +0300

    add registration

--- workspace/SignUpView.swift
+++ workspace/SignUpView.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import iPhoneNumberField
 
 struct SignUpView: View {
     @State private var username = ""
     @State private var email = ""
     @State private var password = ""
     @State private var confirmPassword = ""
+    @State private var fromPhone = false
+    @State private var phone = "0"
+    @State private var errorMess = ""
+    @State private var regError = false
 
     var body: some View {
         VStack {
@@ -19,36 +24,71 @@ struct SignUpView: View {
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .padding(.bottom, 40)
+            if (!fromPhone) {
 
-            TextField("Username", text: $username)
-                .padding(.horizontal)
-                .frame(height: 50)
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(10)
-                .padding(.horizontal)
+                TextField("Username", text: $username)
+                    .padding(.horizontal)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
 
-            TextField("Email", text: $email)
-                .padding(.horizontal)
-                .frame(height: 50)
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(10)
-                .padding(.horizontal)
+                TextField("Email", text: $email)
+                    .padding(.horizontal)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
 
-            SecureField("Password", text: $password)
-                .padding(.horizontal)
-                .frame(height: 50)
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(10)
-                .padding(.horizontal)
+                SecureField("Password", text: $password)
+                    .padding(.horizontal)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
 
-            SecureField("Confirm Password", text: $confirmPassword)
-                .padding(.horizontal)
-                .frame(height: 50)
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(10)
-                .padding(.horizontal)
+                SecureField("Confirm Password", text: $confirmPassword)
+                    .padding(.horizontal)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                
+                if regError {
+                    Text(errorMess)
+                        .font(.subheadline)
+                        .foregroundColor(.red)
+                        .padding(.top, 10)
+                }
+            } else {
+                iPhoneNumberField("Phone number", text: $phone)
+                    .flagHidden(false)
+                    .flagSelectable(true)
+                    .keyboardType(.numberPad)
+                    .padding(.horizontal)
+                    .frame(height: 50)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                    
+            }
+            
 
             Button(action: {
+                register(username: username, email: email, password: password) {result in switch result {
+                case .success(let _isRegistered):
+                    if (_isRegistered) {
+                        errorMess = "User \($username) was registered"
+                    } else {
+                        regError = true
+                        errorMess = "Failed registration"
+                    }
+                case .failure(_):
+                    regError = true
+                    errorMess = "Failed registration"
+                }
+                    
+                }
                            // Действие, которое происходит при нажатии на кнопку регистрации
                 
                        }) {
@@ -66,6 +106,26 @@ struct SignUpView: View {
                        }
 
             Spacer()
+            if (!fromPhone) {
+                Button(action: {fromPhone = true}) {
+                    Text("register with phone number")
+                        .foregroundColor(.green)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .padding(.horizontal)
+                        .background(Color.white)
+                }
+            } else {
+                Button(action: {fromPhone = false}) {
+                    Text("register with phone email")
+                        .foregroundColor(.green)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .padding(.horizontal)
+                        .background(Color.white)
+                }
+            }
+            
         }
         .padding()
     }
